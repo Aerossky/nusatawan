3493571e529cc2b058a1581608a7d599835b9574
Aerossky <92148435+Aerossky@users.noreply.github.com>
2025-04-07 20:43:32 +0700

    feat(database seeder): membuat dummy data (#4)

--- database/migrations/2025_04_07_085451_create_destinations_table.php
+++ database/migrations/2025_04_07_085451_create_destinations_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
             $table->text('description');
             $table->string('city');
             $table->decimal('rating', 3, 2)->default(0);
-            $table->decimal('rating_count', 3, 2)->default(0);
+            $table->decimal('rating_count')->default(0);
             $table->integer('time_minutes');
             $table->decimal('latitude', 10, 8);
             $table->decimal('longitude', 11, 8);
