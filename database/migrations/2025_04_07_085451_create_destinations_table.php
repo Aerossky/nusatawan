b50f077b69ea9a5a28c7f13e65a2162b7ff902f3
Aerossky <riskyradeonrx560@gmail.com>
2025-04-07 16:38:22 +0700

    feat(database): menambahkan skema migrasi awal untuk sistem destinasi wisata
    membuat migrasi awal

---
+++ database/migrations/2025_04_07_085451_create_destinations_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('destinations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('created_by')->constrained('users');
+            $table->foreignId('category_id')->constrained('categories');
+            $table->string('place_name');
+            $table->text('description');
+            $table->string('city');
+            $table->decimal('rating', 3, 2)->default(0);
+            $table->decimal('rating_count', 3, 2)->default(0);
+            $table->integer('time_minutes');
+            $table->decimal('latitude', 10, 8);
+            $table->decimal('longitude', 11, 8);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('destinations');
+    }
+};
