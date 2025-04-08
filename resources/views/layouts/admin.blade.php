c55262904cd8c1c91f626b5dc6b1fb85b7ba846b
Aerossky <riskyradeonrx560@gmail.com>
2025-04-08 22:04:35 +0700

    Feat (Dashboard) : membuat dashboard
    membuat halaman dashboard lengkap dengan BE

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -1,28 +1,36 @@
 <!DOCTYPE html>
-<html lang="id">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Halaman Admin Aplikasi Wisata">
+    <meta name="robots" content="noindex, nofollow">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Admin - Wisata</title>
+
+    <link rel="icon" type="image/png" href="/path/to/favicon.png">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @stack('styles')
 </head>
 
-<body class="bg-background text-dark ">
-
+<body class="bg-background text-dark">
     {{-- Navbar --}}
-    <x-admin.navbar name="joko" email="joko@gmail.com" image="https://cdn-icons-png.flaticon.com/512/149/149071.png" />
+    <x-admin.navbar name="joko" email="joko@gmail.com"
+        image="https://cdn-icons-png.flaticon.com/512/149/149071.png" />
+
     {{-- Aside --}}
     <x-admin.aside />
 
     {{-- Konten Halaman --}}
     <main class="p-4 sm:ml-64">
-        {{-- Content --}}
         <div class="p-4 border-2 border-gray-200 rounded-lg mt-14">
             @yield('content')
         </div>
     </main>
 
+    @stack('scripts')
 </body>
 
 </html>
