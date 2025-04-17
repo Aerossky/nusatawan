9fd2dffbe877349e511c2825f31516c4f98a7c06
Aerossky <riskyradeonrx560@gmail.com>
2025-04-17 16:35:57 +0700

    feat (destination submission)
    setup destination submission

--- resources/views/user/destination-submission.blade.php
+++ resources/views/user/destination-submission.blade.php
@@ -160,7 +160,8 @@
                                 </div>
 
                                 <div>
-                                    <label class="block text-sm font-medium text-gray-700 mb-1" for="images">Foto Destinasi (Maksimal 5
+                                    <label class="block text-sm font-medium text-gray-700 mb-1" for="images">Foto
+                                        Destinasi (Maksimal 5
                                         foto)</label>
                                     <div
                                         class="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-lg">
@@ -192,7 +193,8 @@
                             <!-- Kolom Kanan -->
                             <div class="space-y-6">
                                 <div>
-                                    <label class="block text-sm font-medium text-gray-700 mb-1" for="map">Lokasi di Peta</label>
+                                    <label class="block text-sm font-medium text-gray-700 mb-1" for="map">Lokasi di
+                                        Peta</label>
                                     <p class="text-sm text-gray-500 mb-2">Klik pada peta untuk menentukan lokasi</p>
 
                                     <div class="relative mb-4">
@@ -220,15 +222,25 @@
                                                 class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500"
                                                 readonly required>
                                         </div>
+
+                                        {{-- Kota Kabupaten --}}
+                                        <div>
+                                            <label for="administrative_area"
+                                                class="block text-sm font-medium text-gray-700 mb-1">Kota/Kabupaten</label>
+                                            <input type="text" name="administrative_area" id="administrative_area"
+                                                value="{{ old('administrative_area') }}"
+                                                class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500">
+                                        </div>
+                                        {{-- provinsi --}}
+                                        <div class="">
+                                            <label for="province"
+                                                class="block text-sm font-medium text-gray-700 mb-1">Provinsi</label>
+                                            <input type="text" name="province" id="province"
+                                                value="{{ old('province') }}"
+                                                class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500">
+                                        </div>
                                     </div>
 
-                                    <!-- Tambahkan field kota di bawah lat/long -->
-                                    <div class="mt-4">
-                                        <label for="city"
-                                            class="block text-sm font-medium text-gray-700 mb-1">Kota/Kabupaten</label>
-                                        <input type="text" name="city" id="city" value="{{ old('city') }}"
-                                            class="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-blue-500 focus:border-blue-500">
-                                    </div>
                                 </div>
                             </div>
                         </div>
