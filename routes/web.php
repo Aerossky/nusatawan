30708d4c2b33e8f2d7e9987af034d9ad687d9719
Aerossky <92148435+Aerossky@users.noreply.github.com>
2025-04-20 23:13:15 +0700

    feat : route
    menghapus route yang tidak digunakan

--- routes/web.php
+++ routes/web.php
@@ -62,15 +62,6 @@ Route::prefix('admin')->name('admin.')->group(function () {
         ->names('categories');
 
     // Pengajuan destinasi
-    // Route::get('/pengajuan-destinasi', [AdminDestinationSubmissionController::class, 'index'])->name('destination-submission.index');
-    // Route::get('/pengajuan-destinasi/{destinationSubmission}', [AdminDestinationSubmissionController::class, 'edit'])->name('destination-submission.edit');
-    // Route::delete('/pengajuan-destinasi/{destinationSubmission}', [AdminDestinationSubmissionController::class, 'destroy'])->name('destination-submission.destroy');
-
-    // // approve pengajuan destinasi
-    // Route::post('/pengajuan-destinasi/{destinationSubmission}/approve', [AdminDestinationSubmissionController::class, 'approve'])->name('destination-submission.approve');
-    // // tolak pengajuan destinasi
-    // Route::post('/pengajuan-destinasi/{destinationSubmission}/reject', [AdminDestinationSubmissionController::class, 'reject'])->name('destination-submission.reject');
-
     Route::prefix('pengajuan-destinasi')->name('destination-submission.')->group(function () {
         Route::get('/', [AdminDestinationSubmissionController::class, 'index'])->name('index');
         Route::get('/{destinationSubmission}', [AdminDestinationSubmissionController::class, 'edit'])->name('edit');
