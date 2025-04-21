0107114cc18d29130b46565ce427e965017f143a
Aerossky <92148435+Aerossky@users.noreply.github.com>
2025-04-21 11:41:30 +0700

    Update about.blade.php

--- resources/views/user/about.blade.php
+++ resources/views/user/about.blade.php
@@ -28,7 +28,11 @@
             <p class="text-gray-700 max-w-3xl mx-auto">
                 Platform ini lahir dari kesadaran akan pentingnya informasi akurat tentang destinasi wisata Indonesia.
                 Dikembangkan pertama kali pada 2023 sebagai bagian program Studi Independen Bersertifikat Dicoding Batch 6
-                bersama Aufaa Husniati dan Adriansyah.
+                bersama <a href="https://www.linkedin.com/in/aufaahusniati/"
+                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline" target="_blank">Aufaa
+                    Husniati</a> dan <a href="https://www.linkedin.com/in/adriansyah-anca-197270214/"
+                    class="font-medium text-blue-600 dark:text-blue-500 hover:underline" target="_blank">Adriansyah
+                    Anca</a>.
             </p>
             <p class="text-gray-700 max-w-3xl mx-auto">
                 Awalnya, platform ini hanya menyajikan informasi destinasi wisata secara umum. Kini, sebagai proyek skripsi,
@@ -273,12 +277,13 @@
                             </svg>
                             <svg class="w-5 h-5 inline-block" fill="currentColor" viewBox="0 0 20 20">
                                 <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.
-                                                            <svg class="w-5 h-5 inline-block" fill="currentColor" viewBox="0 0 20 20">
-                                                                                            <path d=" M9.049 2.927c.3-.921
-                                    1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8
-                                    2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0
-                                    00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0
-                                    00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
+                                                                            <svg class="w-5 h-5 inline-block" fill="currentColor" viewBox="0 0 20 20">
+                                                                                                            <path d="
+                                    M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371
+                                    1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54
+                                    1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8
+                                    2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98
+                                    8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z">
                                 </path>
                             </svg>
                             <svg class="w-5 h-5 inline-block" fill="currentColor" viewBox="0 0 20 20">
