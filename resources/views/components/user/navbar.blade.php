97ef03a916d67f07a6252366ed2fa7b0feb5f90b
Aerossky <92148435+Aerossky@users.noreply.github.com>
2025-04-28 19:37:13 +0700

    Update navbar.blade.php

--- resources/views/components/user/navbar.blade.php
+++ resources/views/components/user/navbar.blade.php
@@ -35,8 +35,8 @@
                                     class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">DestinasiKu</a>
                             </li>
                             <li>
-                                <a href="#"
-                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Pengaturan</a>
+                                <a href=""
+                                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100">Favorit</a>
                             </li>
                             <li>
                                 <form method="POST" action="">
