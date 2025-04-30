f590274b3f302a7f0ea224b99d05741987eff323
Aerossky <92148435+Aerossky@users.noreply.github.com>
2025-04-30 15:39:49 +0700

    feat : itinerary
    - membuat tampilan itinerary

--- app/Http/Controllers/User/ItineraryController.php
+++ app/Http/Controllers/User/ItineraryController.php
@@ -3,14 +3,35 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
+use App\Services\ItineraryService;
 use Illuminate\Http\Request;
 
 class ItineraryController extends Controller
 {
     //
+    /**
+     * @var ItineraryService
+     */
+    protected $itineraryService;
 
-    public function index()
+    /**
+     * Constructor
+     */
+    public function __construct(ItineraryService $itineraryService)
     {
-        return view('user.itinerary.index');
+        $this->itineraryService = $itineraryService;
+    }
+
+    public function index(Request $request)
+    {
+        $filters = [
+            'search' => $request->input('search'),
+            'status' => $request->input('status'),
+            'sort' => $request->input('sort', 'newest')
+        ];
+
+        $itineraries = $this->itineraryService->getAllItineraries($filters);
+
+        return view('user.itinerary.index', compact('itineraries', 'filters'));
     }
 }
--- app/Services/ItineraryService.php
+++ app/Services/ItineraryService.php
@@ -2,4 +2,46 @@
 
 namespace App\Services;
 
-class ItineraryService {}
+use App\Models\Itinerary;
+use Illuminate\Support\Facades\Auth;
+
+class ItineraryService
+{
+    public function getAllItineraries(array $filters = [])
+    {
+        $query = Itinerary::where('user_id', Auth::id())
+            ->withCount('itineraryDestinations');
+
+        // Apply search filter
+        if (isset($filters['search']) && !empty($filters['search'])) {
+            $query->where('title', 'like', '%' . $filters['search'] . '%');
+        }
+
+        // Apply status filter
+        if (isset($filters['status']) && !empty($filters['status'])) {
+            $query->where('status', $filters['status']);
+        }
+
+        // Apply sorting
+        if (isset($filters['sort'])) {
+            switch ($filters['sort']) {
+                case 'oldest':
+                    $query->orderBy('created_at', 'asc');
+                    break;
+                case 'title_asc':
+                    $query->orderBy('title', 'asc');
+                    break;
+                case 'title_desc':
+                    $query->orderBy('title', 'desc');
+                    break;
+                default:
+                    $query->orderBy('created_at', 'desc');
+                    break;
+            }
+        } else {
+            $query->orderBy('created_at', 'desc');
+        }
+
+        return $query->paginate(9);
+    }
+}
