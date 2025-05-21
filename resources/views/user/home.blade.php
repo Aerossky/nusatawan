ee3bb2e8d9049489c19b1228414c35bc38832c75
Aerossky <riskyradeonrx560@gmail.com>
2025-05-21 18:35:02 +0700

    Update home.blade.php

--- resources/views/user/home.blade.php
+++ resources/views/user/home.blade.php
@@ -60,9 +60,18 @@
                                         <img src="{{ asset($destination->primaryImage ? 'storage/' . $destination->primaryImage->url : 'images/auth.png') }}"
                                             alt="{{ $destination->place_name }}" class="w-full h-full object-cover">
                                     @else
+                                        {{-- <div class="w-full h-full bg-gray-100 flex items-center justify-center">
+                                            <svg xmlns="http://www.w3.org/2000/svg" class="h-10 w-10 text-gray-300"
+                                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5"
+                                                    d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z" />
+                                            </svg>
+                                        </div> --}}
+
                                         <img src="{{ asset('images/categories/' . $destination->category->name . '.jpg') }}"
                                             alt="{{ $destination->category->name }}" class="w-full h-full object-cover">
                                     @endif
+
                                     <!-- Category Badge -->
                                     <div class="absolute top-3 left-3">
                                         <span
@@ -380,6 +389,7 @@
                                 gambaran umum yang cukup akurat.</p>
                         </div>
                     </details>
+
                 </div>
 
                 <div class="text-center mt-10">
